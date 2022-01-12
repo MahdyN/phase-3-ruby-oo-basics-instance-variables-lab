612823741af5a239830cf991fa915e232d56bbd4
Mahdy Noureddine <mahdy.a.noureddine@gmail.com>
2022-01-12 16:36:14 -0500

    commit tree 7b22af7c470b744a6fac0d26205eef4f69285df7

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
+    # setter method
+    def name=(value)
+        @this_dogs_name = value
+    end
+    
+    # getter method
+    def name
+        @this_dogs_name
+    end
 
 end
+
+
+
